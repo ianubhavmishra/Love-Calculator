ccf3fb72ff55a48fbaee1f3528f26d75c6cb08df
Anubhav mishra <132945554+ianubhavmishra@users.noreply.github.com>
2024-09-15 14:11:22 +0530

    Update script.js
    Now it will play songs according to love percentage

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+let audio = [];
+audio[0] = new Audio();
+audio[0].src = "audios/choo lo.mp3";
+audio[1] = new Audio();
+audio[1].src = "audios/joker.mp3";
+audio[2] = new Audio();
+audio[2].src = "audios/puneet.mp3";
+audio[3] = new Audio();
+audio[3].src = "audios/tumko paya hai.mp3";
+audio[4] = new Audio();
+audio[4].src = "audios/pee loon.mp3"; 
+
 function reEnableBtn() {
-    submit.disabled = false;
     document.querySelector(".result").style.top = "-20px";
     document.querySelector(".result").style.opacity = "0";
+    submit.disabled = false;
 }
-function clicked() {
 
+function pauseAllAudio() {
+    audio.forEach(function(aud) {
+        if (!aud.paused) {
+            aud.pause();
+            aud.currentTime = 0;
+        }
+    });
+}
+
+function clicked() {
+    pauseAllAudio();
     let i;
+
     if (in1.value == "" || in2.value == "") {
         alert("please enter your name")
     }
@@ -14,7 +37,7 @@ function clicked() {
         submit.disabled = true;
         document.querySelector(".result").style.top = "0px";
         document.querySelector(".result").style.opacity = "1";
-        if (in1.value == "Anubhav Mishra") {
+        if (in1.value === "Anubhav Mishra") {
             i = 99;
         }
         else {
@@ -22,11 +45,15 @@ function clicked() {
         }
         percentage.innerHTML = "Loving Percentage => " + i + "%"
         if (i <= 40) {
+            audio[Math.floor(Math.random() * (2 - 0 + 1) + 0)].play();
             result.innerHTML = "Tumse Na Ho Payega.....";
+            
         } else if (i >= 40 && i <= 70) {
+            
             result.innerHTML = "Samay Lagega... try karte raho.";
         } else {
             result.innerHTML = "Aapke pyaar me gir gayi.....";
+            audio[Math.floor(Math.random() * (4 - 3 + 1) + 3)].play();
         }
     }
 
